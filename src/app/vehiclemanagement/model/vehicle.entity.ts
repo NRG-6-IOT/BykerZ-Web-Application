169098de57be81e8dee27ed766f2f49d2f02c63c
sebastianrealcalderon <u20221d964@upc.edu.pe>
2025-10-09 01:45:44 -0500

    fix: vehicle class fixed (again) and added Model class

--- src/app/vehiclemanagement/model/vehicle.entity.ts
+++ src/app/vehiclemanagement/model/vehicle.entity.ts
@@ -1,36 +1,81 @@
 export class Vehicle {
   id: number;
   ownerId: number;
-  vin: string;
-  plateNumber: string;
-  model: string;
-  brand: string;
-  status: string;
+  mechanicId: number;
+  model: Model;
   year: string;
-  color: string;
-  authorizedMechanicId: number;
+  plate: string;
 
   constructor(vehicle: {
-    id: number,
+    id?: number,
     ownerId: number,
-    vin: string,
-    plateNumber: string,
-    model: string,
-    brand: string,
-    status: string,
+    mechanicId: number,
+    model: Model,
     year: string,
-    color: string,
-    authorizedMechanicId: number,
+    plate: string,
   }) {
-    this.id = vehicle.id;
+    this.id = vehicle.id || 0;
     this.ownerId = vehicle.ownerId;
-    this.vin = vehicle.vin;
-    this.plateNumber = vehicle.plateNumber;
+    this.mechanicId = vehicle.mechanicId;
     this.model = vehicle.model;
-    this.brand = vehicle.brand;
-    this.status = vehicle.status;
     this.year = vehicle.year;
-    this.color = vehicle.color;
-    this.authorizedMechanicId = vehicle.authorizedMechanicId;
+    this.plate = vehicle.plate;
   }
+}
+
+export class Model {
+  id: number;
+  brand: string;
+  modelYear: string;
+  originCountry: string;
+  producedAt: string;
+  type: string;
+
+  displacement: string;
+  potency: string;
+  engineTorque: string;
+  weight: string;
+  transmission: string;
+  brakes: string;
+  tank: string;
+  seatHeight: string;
+  consumption: string;
+  price: string;
+
+  constructor(model: {
+    id: number,
+    brand: string,
+    modelYear: string,
+    originCountry: string,
+    producedAt: string,
+    type: string,
+    displacement: string,
+    potency: string,
+    engineTorque: string,
+    weight: string,
+    transmission: string,
+    brakes: string,
+    tank: string,
+    seatHeight: string,
+    consumption: string,
+    price: string,
+  }) {
+    this.id = model.id;
+    this.brand = model.brand;
+    this.modelYear = model.modelYear;
+    this.originCountry = model.originCountry;
+    this.producedAt = model.producedAt;
+    this.type = model.type;
+    this.displacement = model.displacement;
+    this.potency = model.potency;
+    this.engineTorque = model.engineTorque;
+    this.weight = model.weight;
+    this.transmission = model.transmission;
+    this.brakes = model.brakes;
+    this.tank = model.tank;
+    this.seatHeight = model.seatHeight;
+    this.consumption = model.consumption;
+    this.price = model.price;
+  }
+
 }
