de9b2a725e368c8639f9b166980d63cdde9bc0a4
Al3xiel <gabriel280104casas@gmail.com>
2025-10-09 20:12:07 -0500

    feat(subscription): subscription page.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import {DashboardPage} from './public/pages/dashboard-page/dashboard-page';
 import {VehiclesPage} from './vehiclemanagement/pages/vehicles-page/vehicles-page';
 import {VehicleDetailsPage} from './vehiclemanagement/pages/vehicle-details-page/vehicle-details-page';
+import {SubscriptionPage} from './subscription/pages/subscription-page/subscription-page';
 
 export const routes: Routes = [
   { path: '', redirectTo: "/dashboard", pathMatch: 'full' },
@@ -10,7 +11,7 @@ export const routes: Routes = [
   { path: "compare", component: DashboardPage },
   { path: "expenses", component: DashboardPage },
   { path: "membership", component: DashboardPage },
-  { path: "subscriptions", component: DashboardPage },
+  { path: "subscriptions", component: SubscriptionPage },
   { path: "vehicles", component: VehiclesPage },
   { path: "vehicle/:vehicleId", component: VehicleDetailsPage },
 ];
