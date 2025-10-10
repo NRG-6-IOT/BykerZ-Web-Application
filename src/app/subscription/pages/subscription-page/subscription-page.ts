3d4fdd35045623c5978cd8967d54f3ccce61dcfc
Al3xiel <gabriel280104casas@gmail.com>
2025-10-09 20:12:25 -0500

    feat(subscription): subscription page.

---
+++ src/app/subscription/pages/subscription-page/subscription-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subscription-page',
+  imports: [],
+  templateUrl: './subscription-page.html',
+  styleUrl: './subscription-page.css'
+})
+export class SubscriptionPage {
+
+}
