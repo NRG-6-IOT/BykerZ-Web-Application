58265400b1a6a205fd07b8f3adbb4475e81123d0
Pachie <sebaspacheco2003@gmail.com>
2025-10-25 22:12:38 -0500

    docs(iam): add documentation for the methods.

--- src/app/shared/components/base-form.component.ts
+++ src/app/shared/components/base-form.component.ts
@@ -1,10 +1,24 @@
 import {FormGroup} from '@angular/forms';
 
 export class BaseFormComponent {
+  /**
+   * Checks if a control is invalid
+   * @param form The form group to check
+   * @param controlName The name of the control to check
+   * @return True if the control is invalid, false otherwise
+   * @protected
+   */
   protected isInvalidControl(form: FormGroup, controlName: string) {
     return form.controls[controlName].invalid && form.controls[controlName].touched;
   }
 
+  /**
+   * Gets the error messages for a control
+   * @param form The form group to check
+   * @param controlName The name of the control to check
+   * @return The error messages for the control
+   * @protected
+   */
   protected errorMessagesForControl(form: FormGroup, controlName: string) {
     const control = form.controls[controlName];
     let errorMessages = "";
@@ -17,6 +31,13 @@ export class BaseFormComponent {
     return errorMessages;
   }
 
+  /**
+   * Gets the error message for a control and error key
+   * @param controlName The name of the control
+   * @param errorKey The error key
+   * @return The error message
+   * @private
+   */
   private errorMessageForControl(controlName: string, errorKey: string) {
     switch (errorKey) {
       case 'required':
