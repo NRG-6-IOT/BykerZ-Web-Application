590e421523b5e05cc63606f21d2f9a624c39b7e1
Pachie <sebaspacheco2003@gmail.com>
2025-10-25 21:53:28 -0500

    feat(iam): add sign up and sign in methods to authentication service.

--- src/app/iam/services/authentication.service.ts
+++ src/app/iam/services/authentication.service.ts
@@ -2,6 +2,10 @@ import {environment} from '../../../environments/environment';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {BehaviorSubject} from 'rxjs';
 import {Router} from '@angular/router';
+import {SignUpRequest} from '@app/iam/model/sign-up.request';
+import {SignUpResponse} from '@app/iam/model/sign-up.response';
+import {SignInRequest} from '@app/iam/model/sign-in.request';
+import {SignInResponse} from '@app/iam/model/sign-in-response';
 
 export class AuthenticationService {
   baseUrl: string = `${environment.serverBaseUrl}`;
@@ -25,4 +29,58 @@ export class AuthenticationService {
     return this.signedInUsername.asObservable();
   }
 
+  /**
+   * Sign up a new user.
+   * @param signUpRequest
+   * @return The sign up response.
+   */
+  signUp(signUpRequest: SignUpRequest) {
+    return this.http.post<SignUpResponse>(`${this.baseUrl}/authentication/sign-up`, signUpRequest, this.httpOptions)
+      .subscribe({
+        next: (response) => {
+          console.log(`Signed up as ${response.username}`);
+          this.router.navigate(['/sign-in']).then();
+        },
+        error: (error) => {
+          console.error(`Error while signing up: ${error}`);
+          this.router.navigate(['/sign-up']).then();
+        }
+      });
+  }
+
+  /**
+   * Sign in an existing user.
+   * @param signInRequest
+   * @return The sign in response.
+   */
+  signIn(signInRequest: SignInRequest) {
+    return this.http.post<SignInResponse>(`${this.baseUrl}/authentication/sign-in`, signInRequest, this.httpOptions)
+      .subscribe({
+        next: (response) => {
+          this.signedIn.next(true);
+          this.signedInUserId.next(response.id);
+          this.signedInUsername.next(response.username);
+          localStorage.setItem('token', response.token);
+          this.router.navigate(['/dashboard']).then();
+        },
+        error: (error) => {
+          this.signedIn.next(false);
+          this.signedInUserId.next(0);
+          this.signedInUsername.next('');
+          console.error(`Error while signing in: ${error}`);
+          this.router.navigate(['/sign-in']).then();
+        }
+      });
+  }
+
+  /**
+   * Sign out the current user.
+   */
+  signOut() {
+    this.signedIn.next(false);
+    this.signedInUserId.next(0);
+    this.signedInUsername.next('');
+    localStorage.removeItem('token');
+    this.router.navigate(['/sign-in']).then();
+  }
 }
