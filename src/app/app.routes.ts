93dcb1cf6dc7f61013a839378f036bfc987e0b91
Pachie <sebaspacheco2003@gmail.com>
2025-10-25 22:55:52 -0500

    feat(iam): add routes for sign-in and sign-up pages.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,22 +5,21 @@ import {ExpensesPageComponent} from '@app/wellness/pages/expenses-page/expenses-
 import {ExpenseItemComponent} from '@app/wellness/components/expense-item/expense-item.component';
 import {VehiclesPage} from './vehiclemanagement/pages/vehicles-page/vehicles-page';
 import {VehicleDetailsPage} from './vehiclemanagement/pages/vehicle-details-page/vehicle-details-page';
-import {AssignmentPage} from '@app/assignments/pages/assignment-page/assignment-page.component';
-import {AssignmentDetailPage} from '@app/assignments/pages/assignment-detail-page/assignment-detail-page.component';
-import {ComparePageComponent} from '@app/comparatives/pages/compare-page/compare-page.component';
-import {CompareMechanicComponent} from '@app/comparatives/pages/compare-mechanic/compare-mechanic.component';
+import {SubscriptionPage} from './subscription/pages/subscription-page/subscription-page';
+import {SignInPage} from '@app/iam/pages/sign-in/sign-in.page';
+import {SignUpPage} from '@app/iam/pages/sign-up/sign-up.page';
 
 export const routes: Routes = [
   { path: '', redirectTo: "/dashboard", pathMatch: 'full' },
+  { path: "sign-in", component: SignInPage },
+  { path: "sign-up", component: SignUpPage },
   { path: "dashboard", component: DashboardPage},
   { path: "maintenances", component: MaintenanceComponent },
-  { path: "compare", component: ComparePageComponent },
-  { path: "compare/mechanic", component: CompareMechanicComponent },
+  { path: "compare", component: DashboardPage },
   { path: "expenses", component: ExpensesPageComponent },
   { path: "expenses/:id" , component: ExpenseItemComponent },
   { path: "membership", component: DashboardPage },
-  { path: "assignments", component: AssignmentPage },
-  { path: "assignment/:id", component: AssignmentDetailPage },
+  { path: "subscriptions", component: SubscriptionPage },
   { path: "vehicles", component: VehiclesPage },
   { path: "vehicle/:vehicleId", component: VehicleDetailsPage },
 ];
