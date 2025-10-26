485987b77ae0bb5e09c9814aefc0a621be763b9a
Pachie <sebaspacheco2003@gmail.com>
2025-10-26 11:01:46 -0500

    feat(iam): add sign-in page with form and validation.

---
+++ src/app/iam/pages/sign-in/sign-in.page.ts
@@ -0,0 +1,50 @@
+import {Component, OnInit} from '@angular/core';
+import {BaseFormComponent} from '@app/shared/components/base-form.component';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+//import {AuthenticationService} from '@app/iam/services/authentication.service';
+import {SignInRequest} from '@app/iam/model/sign-in.request';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {RouterLink} from '@angular/router';
+import {NgOptimizedImage} from '@angular/common';
+
+@Component({
+  selector: 'app-sign-in',
+  imports: [
+    ReactiveFormsModule,
+    FormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    RouterLink,
+    NgOptimizedImage
+  ],
+  templateUrl: './sign-in.page.html',
+  styleUrl: './sign-in.page.css'
+})
+export class SignInPage extends BaseFormComponent implements OnInit {
+  signInFormGroup!: FormGroup;
+  submitted = false;
+
+  constructor(private builder: FormBuilder/*, private authenticationService: AuthenticationService*/) {
+    super();
+  }
+
+  ngOnInit() {
+    this.signInFormGroup = this.builder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  signIn() {
+    if (this.signInFormGroup.invalid) return;
+    let username = this.signInFormGroup.value.username;
+    let password = this.signInFormGroup.value.password;
+    //const signInRequest = new SignInRequest(username, password);
+    //this.authenticationService.signIn(signInRequest);
+    console.log(`Sign in requested for user: ${username} with password: ${password} (TBD)`);
+    this.submitted = true;
+  }
+}
