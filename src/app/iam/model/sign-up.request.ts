f1c0f853c159046a40763c6420325dcc01f5d015
Pachie <sebaspacheco2003@gmail.com>
2025-10-28 01:13:47 -0500

    feat(iam): extend SignUpRequest class to include additional fields.

--- src/app/iam/model/sign-up.request.ts
+++ src/app/iam/model/sign-up.request.ts
@@ -1,9 +1,26 @@
 export class SignUpRequest {
+  public firstName: string;
+  public lastName: string;
   public username: string;
+  public email: string;
+  public photoUrl: string;
   public password: string;
+  public role: string
 
-  constructor(username: string, password: string) {
+  constructor(
+    firstName: string,
+    lastName: string,
+    username: string,
+    email: string,
+    photoUrl: string,
+    password: string,
+    role: string){
+    this.firstName = firstName;
+    this.lastName = lastName;
     this.username = username;
+    this.email = email;
+    this.photoUrl = photoUrl;
     this.password = password;
+    this.role = role;
   }
 }
