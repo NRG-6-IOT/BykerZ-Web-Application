7583461364f09ac97ca91acf91be1eb3647ce14e
cwassointt <u2021120@upc.edu.pe>
2025-10-31 12:11:55 -0500

    feat: remove unused imports from dashboard page component

---
+++ src/app/public/pages/dashboard-page/dashboard-page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-page',
+  templateUrl: './dashboard-page.html',
+  standalone: true,
+  styleUrl: './dashboard-page.css'
+})
+export class DashboardPage {
+}
