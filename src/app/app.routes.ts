272633fcb37c81db169775acc0375222d1593d71
cwassointt <u2021120@upc.edu.pe>
2025-10-31 11:28:46 -0500

    feat: add route for CompareMechanicComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,12 +7,14 @@ import {VehiclesPage} from './vehiclemanagement/pages/vehicles-page/vehicles-pag
 import {VehicleDetailsPage} from './vehiclemanagement/pages/vehicle-details-page/vehicle-details-page';
 import {SubscriptionPage} from './subscription/pages/subscription-page/subscription-page';
 import { ComparePageComponent } from './comparatives/pages/compare-page/compare-page.component';
+import {CompareMechanicComponent} from '@app/comparatives/pages/compare-mechanic/compare-mechanic.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: "/dashboard", pathMatch: 'full' },
   { path: "dashboard", component: DashboardPage},
   { path: "maintenances", component: MaintenanceComponent },
   { path: "compare", component: ComparePageComponent },
+  { path: "compare/mechanic", component: CompareMechanicComponent },
   { path: "expenses", component: ExpensesPageComponent },
   { path: "expenses/:id" , component: ExpenseItemComponent },
   { path: "membership", component: DashboardPage },
