f4ceb6eafb26b2de9bef3f97f5f9e49a43155fc9
cwassointt <u202112078@upc.edu.pe>
2025-10-31 12:15:02 -0500

    feat(assignments): add notifications drop down list.

---
+++ src/app/assignments/components/notification-dropdown/notification-dropdown.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import {NgForOf} from '@angular/common';
+
+@Component({
+  selector: 'app-notification-dropdown',
+  templateUrl: './notification-dropdown.component.html',
+  imports: [
+    NgForOf
+  ],
+  styleUrls: ['./notification-dropdown.component.css']
+})
+export class NotificationDropdownComponent {
+  @Input() items: string[] = [
+    'Notificación 1',
+    'Notificación 2',
+    'Notificación 3'
+  ];
+}
