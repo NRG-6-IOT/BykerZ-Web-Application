31cff930a284d57019020cfcb7dca3ea26d4ddf0
Pachie <sebaspacheco2003@gmail.com>
2025-11-04 14:59:05 -0500

    fix(iam): add HTTP client with authentication interceptor to app configuration.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,14 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { provideNativeDateAdapter } from '@angular/material/core';
-import { authInterceptor } from './core/interceptors/auth.interceptor';
+import {provideNativeDateAdapter} from '@angular/material/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideNativeDateAdapter(),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideHttpClient(
-      withInterceptors([authInterceptor])
-    )
+    provideRouter(routes)
   ]
 };
