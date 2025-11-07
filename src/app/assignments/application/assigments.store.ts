45cb4d20049390892351c3d5f0b4884e76cf4581
Al3xiel <gabriel280104casas@gmail.com>
2025-11-07 00:36:33 -0500

    feat(assignments): implement loading and error handling in assignments page and bounded context storage.

---
+++ src/app/assignments/application/assigments.store.ts
@@ -0,0 +1,47 @@
+import {Injectable, signal} from "@angular/core";
+import {Assignment} from '@app/assignments/domain/model/assignment.entity';
+import {delay} from 'rxjs';
+import {Owner} from '@app/assignments/domain/model/owner.entity';
+
+@Injectable({providedIn: 'root'})
+export class AssignmentsStore {
+  private assignmentsSignal = signal<Assignment[]>([]);
+  private readonly errorSignal = signal<string | null>(null);
+  private readonly loadingSignal = signal<boolean>(false);
+
+  readonly assignments = this.assignmentsSignal.asReadonly();
+  readonly error = this.errorSignal.asReadonly();
+  readonly loading = this.loadingSignal.asReadonly();
+
+  constructor() {
+    this.loadAssignments();
+  }
+
+  private loadAssignments(): void {
+    this.loadingSignal.set(true);
+    this.errorSignal.set(null);
+    setTimeout(() => {
+      try {
+        const fetchedAssignments: Assignment[] = [
+          new Assignment(1, 'Frequent Customer', new Owner(1, 'Ana Torres'), 2),
+          new Assignment(2, 'Regular Customer', new Owner(2, 'Luis Pérez'), 1),
+          new Assignment(3, 'Business Customer', new Owner(3, 'María Gómez'), 3),
+          new Assignment(4, 'New Customer', new Owner(4, 'Carlos Ruiz'), 4),
+          new Assignment(5, 'Occasional Customer', new Owner(5, 'Elena Díaz'), 2),
+        ];
+        this.assignmentsSignal.set(fetchedAssignments);
+      } catch (err) {
+        this.errorSignal.set(this.formatError(err, 'Error cargando assignments'));
+      } finally {
+        this.loadingSignal.set(false);
+      }
+    }, 2000);
+  }
+
+  private formatError(error: any, fallback: string): string {
+    if(error instanceof Error) {
+      return error.message.includes('Resource not found') ? `${fallback}: Not found` : error.message;
+    }
+    return fallback;
+  }
+}
--- src/app/assignments/presentation/views/assignments-page/assignments-page.ts
+++ src/app/assignments/presentation/views/assignments-page/assignments-page.ts
@@ -1,29 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {NotificationButton} from '@app/assignments/presentation/components/notification-button/notification-button';
-import {Assignment} from '@app/assignments/domain/model/assignment.entity';
 import {AssignmentCardList} from '@app/assignments/presentation/components/assignment-card-list/assignment-card-list';
-import {Owner} from '@app/assignments/domain/model/owner.entity';
+import {AssignmentsStore} from '@app/assignments/application/assigments.store';
+import {MatProgressSpinner} from '@angular/material/progress-spinner';
+import {MatError} from '@angular/material/form-field';
 
 
 @Component({
   selector: 'app-assignments-page',
   imports: [
     NotificationButton,
-    AssignmentCardList
+    AssignmentCardList,
+    MatProgressSpinner,
+    MatError
   ],
   templateUrl: './assignments-page.html',
   styleUrl: './assignments-page.css'
 })
 export class AssignmentsPage {
-  assignments: Array<Assignment> = [];
+  readonly store = inject(AssignmentsStore);
 
-  ngOnInit(): void {
-    this.assignments = [
-      new Assignment(1, 'Frequent Customer', new Owner(1, 'Ana Torres'), 2),
-      new Assignment(2, 'Regular Customer', new Owner(2, 'Luis Pérez'), 1),
-      new Assignment(3, 'Business Customer', new Owner(3, 'María Gómez'), 3),
-      new Assignment(4, 'New Customer', new Owner(4, 'Carlos Ruiz'), 4),
-      new Assignment(5, 'Occasional Customer', new Owner(5, 'Elena Díaz'), 2),
-    ];
+  get assignments() {
+    return this.store.assignments();
   }
+
 }
