d9f44c2a4f4f9040ad6f3083ca7a0453b0387135
Al3xiel <gabriel280104casas@gmail.com>
2025-11-09 02:07:03 -0500

    feat(assignments): refactor assignments store and card components to handle active and pending assignments

--- src/app/assignments/application/assigments.store.ts
+++ src/app/assignments/application/assigments.store.ts
@@ -7,25 +7,28 @@ import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 
 @Injectable({providedIn: 'root'})
 export class AssignmentsStore {
-  private assignmentsSignal = signal<Assignment[]>([]);
+  private activeAssignmentsSignal = signal<Assignment[]>([]);
+  private pendingAssignmentsSignal = signal<Assignment[]>([]);
   private readonly errorSignal = signal<string | null>(null);
   private readonly loadingSignal = signal<boolean>(false);
 
-  readonly assignments = this.assignmentsSignal.asReadonly();
+  readonly activeAssignments = this.activeAssignmentsSignal.asReadonly();
+  readonly pendingAssignments = this.pendingAssignmentsSignal.asReadonly();
   readonly error = this.errorSignal.asReadonly();
   readonly loading = this.loadingSignal.asReadonly();
 
   constructor(private assignmentsApi: AssignmentsApi) {
-    this.loadAssignments();
+    this.loadActiveAssignments();
+    this.loadPendingAssignments()
   }
 
-  private loadAssignments(): void {
+  private loadActiveAssignments(): void {
     this.loadingSignal.set(true);
     this.errorSignal.set(null);
     this.assignmentsApi.getAssignmentsByMechanicAndStatus(1,'ACTIVE').pipe(takeUntilDestroyed()).subscribe(
       {
         next: assignments =>{
-         this.assignmentsSignal.set(assignments);
+         this.activeAssignmentsSignal.set(assignments);
           this.loadingSignal.set(false);
         },
         error: err => {
@@ -34,24 +37,23 @@ export class AssignmentsStore {
         }
       }
     )
-    /*
-    setTimeout(() => {
-      try {
-        const fetchedAssignments: Assignment[] = [
-          new Assignment(1, 'Frequent Customer', new Owner(1, 'Ana Torres'), 2),
-          new Assignment(2, 'Regular Customer', new Owner(2, 'Luis Pérez'), 1),
-          new Assignment(3, 'Business Customer', new Owner(3, 'María Gómez'), 3),
-          new Assignment(4, 'New Customer', new Owner(4, 'Carlos Ruiz'), 4),
-          new Assignment(5, 'Occasional Customer', new Owner(5, 'Elena Díaz'), 2),
-        ];
-        this.assignmentsSignal.set(fetchedAssignments);
-      } catch (err) {
-        this.errorSignal.set(this.formatError(err, 'Error cargando assignments'));
-      } finally {
-        this.loadingSignal.set(false);
+  }
+
+  private loadPendingAssignments(): void {
+    this.loadingSignal.set(true);
+    this.errorSignal.set(null);
+    this.assignmentsApi.getAssignmentsByMechanicAndStatus(1,'PENDING').pipe(takeUntilDestroyed()).subscribe(
+      {
+        next: assignments =>{
+          this.pendingAssignmentsSignal.set(assignments);
+          this.loadingSignal.set(false);
+        },
+        error: err => {
+          this.errorSignal.set(this.formatError(err, 'Failed to load courses'));
+          this.loadingSignal.set(false);
+        }
       }
-    }, 2000);
-     */
+    )
   }
 
   private formatError(error: any, fallback: string): string {
