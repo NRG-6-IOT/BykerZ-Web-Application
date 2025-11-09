72fe3e95bd442f2e78c31857ada37226c95a9fc1
Al3xiel <gabriel280104casas@gmail.com>
2025-11-09 14:10:54 -0500

    feat(public): refactor dashboard and navbar component imports for improved structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import {DashboardPage} from './public/pages/dashboard-page/dashboard-page';
+import {DashboardPage} from '@app/public/presentation/views/dashboard-page/dashboard-page';
 import {MaintenanceComponent} from '@app/wellness/pages/maintenence/maintenance.component';
 import {ExpensesPageComponent} from '@app/wellness/pages/expenses-page/expenses-page.component';
 import {ExpenseItemComponent} from '@app/wellness/components/expense-item/expense-item.component';
