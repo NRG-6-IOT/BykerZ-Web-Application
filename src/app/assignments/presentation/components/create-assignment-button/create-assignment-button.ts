8890dbe17bc5fcaa2470f9ed92c6dd9f2908f250
Al3xiel <gabriel280104casas@gmail.com>
2025-11-09 04:16:15 -0500

    feat(assignments): add create assignment dialog and integrate with store

--- src/app/assignments/presentation/components/create-assignment-button/create-assignment-button.ts
+++ src/app/assignments/presentation/components/create-assignment-button/create-assignment-button.ts
@@ -1,17 +1,28 @@
 import {Component, inject} from '@angular/core';
 import {AssignmentsStore} from '@app/assignments/application/assigments.store';
+import {Assignment} from '@app/assignments/domain/model/assignment.entity';
+import {
+  CreateAssignmentDialog
+} from '@app/assignments/presentation/components/create-assignment-dialog/create-assignment-dialog';
 
 @Component({
   selector: 'app-create-assignment-button',
   imports: [
+    CreateAssignmentDialog
   ],
   templateUrl: './create-assignment-button.html',
   styleUrl: './create-assignment-button.css'
 })
 export class CreateAssignmentButton {
   readonly store = inject(AssignmentsStore);
+  showDialog = false;
 
   openDialog() {
     this.store.createAssignment(1);
+    this.showDialog = true;
+  }
+
+  closeDialog() {
+    this.showDialog = false;
   }
 }
---
+++ src/app/assignments/presentation/components/create-assignment-dialog/create-assignment-dialog.ts
@@ -0,0 +1,23 @@
+import {Component, EventEmitter, inject, Input, OnInit, Output} from '@angular/core';
+import {Assignment} from '@app/assignments/domain/model/assignment.entity';
+import {AssignmentsStore} from '@app/assignments/application/assigments.store';
+
+@Component({
+  selector: 'app-create-assignment-dialog',
+  imports: [
+  ],
+  templateUrl: './create-assignment-dialog.html',
+  styleUrl: './create-assignment-dialog.css'
+})
+export class CreateAssignmentDialog {
+  readonly store = inject(AssignmentsStore);
+  @Output() close = new EventEmitter<void>();
+
+  get createdAssigment() {
+    return this.store.createdAssignment();
+  }
+
+  onClose() {
+    this.close.emit();
+  }
+}
