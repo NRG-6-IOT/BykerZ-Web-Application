95eb461e3e5b1717410bea1bb100fddbaa8f4c7d
Al3xiel <gabriel280104casas@gmail.com>
2025-11-09 00:40:44 -0500

    chore: update base URL to platform provider API and add new endpoint paths

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: false,
-  serverBaseUrl: "http://localhost:8080/api/v1",
+  platformProviderApiBaseUrl: "http://localhost:8080/api/v1",
+  platformProviderAssignmentsEndpointPath: '/assignments',
+  platformProviderMechanicEndpointPath: '/mechanic',
+  platformProviderOwnerEndpointPath: '/owner',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
-  serverBaseUrl: "http://localhost:8080/api/v1",
+  platformProviderApiBaseUrl: "http://localhost:8080/api/v1",
+  platformProviderAssignmentsEndpointPath: '/assignments',
+  platformProviderMechanicEndpointPath: '/mechanic',
+  platformProviderOwnerEndpointPath: '/owner',
 };
