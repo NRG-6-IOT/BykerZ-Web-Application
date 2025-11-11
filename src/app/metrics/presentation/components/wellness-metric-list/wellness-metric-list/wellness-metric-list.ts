b2022fd38b71063f27a98952f5a8669b00283407
Firtness <u202122484@upc.edu.pe>
2025-11-11 03:17:05 -0500

    feat(wellnessMetric): added wellnessMetric

---
+++ src/app/metrics/presentation/components/wellness-metric-list/wellness-metric-list/wellness-metric-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wellness-metric-list',
+  imports: [],
+  templateUrl: './wellness-metric-list.html',
+  styleUrl: './wellness-metric-list.css'
+})
+export class WellnessMetricList {
+
+}
