2e1a209ec06f54512b42a19d9b2023682e613d3b
sebastianrealcalderon <u20221d964@upc.edu.pe>
2025-11-15 00:26:52 -0500

    fix: wellness imports

--- src/app/vehicle-wellness/presentation/components/wellness-metric-item/wellness-metric-item.ts
+++ src/app/vehicle-wellness/presentation/components/wellness-metric-item/wellness-metric-item.ts
@@ -1,9 +1,7 @@
-import {Component, input, Input, output} from '@angular/core';
+import {Component, inject, input, Input, output} from '@angular/core';
 import {WellnessMetric} from '@app/vehicle-wellness/domain/model/wellness-metric.entity';
 import {MatListItem} from '@angular/material/list';
 import {DatePipe} from '@angular/common';
-import {Component, inject, input, Input, output} from '@angular/core';
-import {WellnessMetric} from '@app/metrics/domain/model/wellness-metric.entity';
 import {VehiclesStore} from '@app/vehiclemanagement/application/vehicles.store';
 import {Vehicle} from '@app/vehiclemanagement/domain/model/vehicle.entity';
 
