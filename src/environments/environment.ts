c3de6e6ba3e9886b0824331e4231da3f75f0b7ef
NeoRise456 <defneorise@gmail.com>
2025-11-15 00:50:43 -0500

    fix(environment): update platformProviderApiBaseUrl to production URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  platformProviderApiBaseUrl: "http://localhost:8080/api/v1",
+  platformProviderApiBaseUrl: "https://bykerz-backend.onrender.com/api/v1",
   platformProviderAssignmentsEndpointPath: '/assignments',
   platformProviderMechanicEndpointPath: '/mechanic',
   platformProviderOwnerEndpointPath: '/owner',
