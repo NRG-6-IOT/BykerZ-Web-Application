d883fada1c655049d2d87e6030a27afbcd80e588
sebastianrealcalderon <u20221d964@upc.edu.pe>
2025-11-14 22:52:59 -0500

    feat: wellness metric owner and vehicle name

--- src/app/metrics/presentation/components/wellness-metric-item/wellness-metric-item.ts
+++ src/app/metrics/presentation/components/wellness-metric-item/wellness-metric-item.ts
@@ -1,5 +1,7 @@
-import {Component, input, Input, output} from '@angular/core';
+import {Component, inject, input, Input, output} from '@angular/core';
 import {WellnessMetric} from '@app/metrics/domain/model/wellness-metric.entity';
+import {VehiclesStore} from '@app/vehiclemanagement/application/vehicles.store';
+import {Vehicle} from '@app/vehiclemanagement/domain/model/vehicle.entity';
 
 @Component({
   selector: 'app-wellness-metric-item',
@@ -12,6 +14,12 @@ export class WellnessMetricItem {
 
   wellnessMetric = input.required<WellnessMetric>();
   wellnessMetricSelected = output<WellnessMetric>();
+
+  vehicleStore = inject(VehiclesStore)
+
+  getVehicle(): Vehicle {
+    return this.vehicleStore.getVehicleById(this.wellnessMetric().vehicleId)()!;
+  }
 
   emitMetricSelectedEvent() {
     this.wellnessMetricSelected.emit(this.wellnessMetric());
