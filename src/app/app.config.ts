7bc47023aca148b3f1aad316d3d25a0d57ad0b7d
Al3xiel <gabriel280104casas@gmail.com>
2025-11-23 20:30:47 -0500

    feat(i18n): integrate ngx-translate for dynamic language support

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,32 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  APP_INITIALIZER,
+  ApplicationConfig,
+  importProvidersFrom,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import {provideNativeDateAdapter} from '@angular/material/core';
-import {provideHttpClient, withInterceptors} from '@angular/common/http';
+import {HttpClient, provideHttpClient, withInterceptors} from '@angular/common/http';
 import {authenticationInterceptor} from '@app/iam/services/authentication.interceptor';
+import {TranslateLoader, TranslateModule, TranslateService} from '@ngx-translate/core';
+import {firstValueFrom} from 'rxjs';
+
+export function HttpLoaderFactory(http: HttpClient): TranslateLoader {
+  return {
+    getTranslation: (lang: string) => http.get(`/assets/i18n/${lang}.json`)
+  } as TranslateLoader;
+}
+
+export function appInitializerFactory(translate: TranslateService) {
+  return () => {
+    translate.setDefaultLang('en');
+    return firstValueFrom(translate.use('en'));
+  };
+}
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,6 +34,16 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authenticationInterceptor]))
+    provideHttpClient(withInterceptors([authenticationInterceptor])),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient],
+        }
+      })
+    ),
+    { provide: APP_INITIALIZER, useFactory: appInitializerFactory, deps: [TranslateService], multi: true }
   ]
 };
