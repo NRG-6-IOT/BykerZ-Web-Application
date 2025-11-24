4950a715d8d642522fc1cb6088658be36c3d53ca
Al3xiel <gabriel280104casas@gmail.com>
2025-11-24 00:29:17 -0500

    feat(i18n): add translation for no active assignments message in English and Spanish

--- src/app/assignments/presentation/components/assignment-card-list/assignment-card-list.ts
+++ src/app/assignments/presentation/components/assignment-card-list/assignment-card-list.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from '@angular/core';
 import {Assignment} from '@app/assignments/domain/model/assignment.entity';
 import {AssignmentCard} from '@app/assignments/presentation/components/assignment-card/assignment-card';
+import {TranslatePipe} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-assignment-card-list',
   imports: [
-    AssignmentCard
+    AssignmentCard,
+    TranslatePipe
   ],
   templateUrl: './assignment-card-list.html',
   styleUrl: './assignment-card-list.css'
