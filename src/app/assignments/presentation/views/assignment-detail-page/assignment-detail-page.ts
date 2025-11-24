ce11f76b3364a1927924dae3ab5c20baf72b25fb
Al3xiel <gabriel280104casas@gmail.com>
2025-11-24 00:27:49 -0500

    feat(i18n): remove unused assignment card dialog.

--- src/app/assignments/presentation/views/assignment-detail-page/assignment-detail-page.ts
+++ src/app/assignments/presentation/views/assignment-detail-page/assignment-detail-page.ts
@@ -1,16 +1,11 @@
 import {Component, inject, OnInit} from '@angular/core';
 import {Assignment} from '@app/assignments/domain/model/assignment.entity';
 import {ActivatedRoute} from '@angular/router';
-import {
-  AssignmentCardDialog
-} from '@app/assignments/presentation/components/assignment-card-dialog/assignment-card-dialog';
-import {MatDialog} from '@angular/material/dialog';
 import {AssignmentsStore} from '@app/assignments/application/assigments.store';
 import {
   AssignmentTypeSelector
 } from '@app/assignments/presentation/components/assignment-type-selector/assignment-type-selector';
 import {VehiclesStore} from '@app/vehiclemanagement/application/vehicles.store';
-import {VehicleCard} from '@app/vehiclemanagement/presentation/components/vehicle-card/vehicle-card';
 import {AssignVehicleCard} from '@app/assignments/presentation/components/assign-vehicle-card/assign-vehicle-card';
 
 @Component({
